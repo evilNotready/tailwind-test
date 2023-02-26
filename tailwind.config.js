9883a70c41db4c3bdeb3b9836cae9ecbc7ad2392
evilNotready <bogatov.pasha78@gmail.com>
2023-02-26 18:54:30 +0500

    washinima

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./**/*.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        mainColor: '#050C2A'
+      },
+      screens: {
+        sm: '400px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1440px'
+      }
+    },
+  },
+  plugins: [],
+}
